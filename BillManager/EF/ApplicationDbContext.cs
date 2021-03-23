c67f4d1b90d0bcc447fb6946a7bca5d6a12b4192
M-Peplin <m.p.peplinski@gmail.com>
2021-03-23 20:10:03 +0100

    Added ApplicationUserConfiguration. Modified DbContext class.

--- BillManager/EF/ApplicationDbContext.cs
+++ BillManager/EF/ApplicationDbContext.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BillManager.Models;
+using BillManager.Models.ModelConfiguration;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BillManager.EF
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
+            
         }
-        
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<Information> Information { get; set; }
+        public DbSet<Bill> Bill { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new ApplicationUserConfiguration());
+        }
+
     }
 }
