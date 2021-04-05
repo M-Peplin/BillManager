5b73e585e7c7189b66ebc37a771b1d79d4441c95
M-Peplin <m.p.peplinski@gmail.com>
2021-04-05 17:30:26 +0200

    Added model configurations (Bill and Information).

---
+++ BillManager/Models/ModelConfiguration/BillConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BillManager.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BillManager.Models.ModelConfiguration
+{
+    public class BillConfiguration : IEntityTypeConfiguration<Bill>
+    {
+        public void Configure(EntityTypeBuilder<Bill> entity)
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired();
+            entity.HasOne<ApplicationUser>(u => u.User).WithMany(b => b.Bills).HasForeignKey(b => b.UserId);
+        }
+    }
+}
---
+++ BillManager/Models/ModelConfiguration/InformationsConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillManager.Models.ModelConfiguration
+{
+    public class InformationsConfiguration : IEntityTypeConfiguration<Information>
+    {
+        public void Configure(EntityTypeBuilder<Information> entity)
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired();
+            entity.HasOne<ApplicationUser>(a => a.User).WithMany(i => i.Informations).HasForeignKey(u => u.UserId);
+        }
+    }
+}
