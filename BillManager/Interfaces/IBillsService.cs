968b9decdfc23c60240842dd371504fff9d3a05f
M-Peplin <m.p.peplinski@gmail.com>
2021-05-11 22:00:04 +0200

    Added interfaces.

---
+++ BillManager/Interfaces/IBillsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BillManager.Models.ModelsDTO;
+
+namespace BillManager.Interfaces
+{
+    public interface IBillsService
+    {
+        ResponseDTO AddBill(BillDTO billDTO);
+        ResponseDTO EditBIll(BillDTO billDTO);
+        ResponseDTO DeleteBill(string mail);
+        BillsDTO GetAllBillByUser(string mail);
+    }
+}
