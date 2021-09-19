7e1723acba080afb11d75f8a6bc9ca93bbf1b5ba
M-Peplin <m.p.peplinski@gmail.com>
2021-09-19 19:09:51 +0200

    Added Users Controller

---
+++ BillManager/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using BillManager.Interfaces;
+using BillManager.Models.ModelsDTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillManager.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly IUsersService _usersService;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(IUsersService usersService, ILogger<UsersController> logger)
+        {
+            this._usersService = usersService;
+            this._logger = logger;
+        }
+        [HttpGet]
+        [Route("/api/users/getAll")]
+        public IActionResult GetAllUsers()
+        {
+            _logger.LogInformation("Executing GetAllUsers controller");
+            return Ok(_usersService.GetAllUsers());
+        }
+        [HttpPut]
+        [Route("api/users/edit")]
+        public ActionResult EditUser([FromBody] UserDTO userDTO)
+        {
+            _logger.LogInformation("Executing EditUser controller");
+            return Ok(_usersService.EditUser(userDTO));
+        }
+
+    }
+}
